4ff92cbf9306d90d48cd05a6f9ab61742cff61c2
cyw <1764887758@qq.com>
2023-09-08 17:09:36 +0800

    添加organizationmodel

---
+++ src/Models/Organization.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Porygon\Organization\Models;
+
+use Dcat\Admin\Traits\ModelTree;
+use Laravel\Sanctum\HasApiTokens;
+use Porygon\Base\Models\Model;
+use Porygon\User\Models\User;
+use Spatie\EloquentSortable\Sortable;
+
+class Organization extends Model
+{
+}
